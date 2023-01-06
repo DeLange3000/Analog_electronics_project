d597b2ba602500c999b68a4f0904877388149210
Rogier De Nys <rogierdenys@gmail.com>
2023-01-06 13:56:20 +0100

    mijn versie (volledig) maar werkt niet in ltspice

--- Matlab/part_1_project_nmos.m
+++ Matlab/part_1_project_nmos.m
@@ -39,23 +39,25 @@ fprintf('\n--- First Exercise: Designing transistor from scratch ---\n');
 
 
 %% Intrinsic gain versus VGS for different L
-VGS = (0:0.025:spec.VDD).'; % [V], gate source voltage
-L   = [60 80 100 200 500 1000 1500]*1e-9; % [m], gate length
+VGS = (0:0.005:spec.VDD).'; % [V], gate source voltage
+L   = [60 80 100 200 500 1000 2000]*1e-9; % [m], gate length
 gm  = NaN(length(L),length(VGS));
 gds = NaN(length(L),length(VGS));
 gmIDS = NaN(length(L),length(VGS));
+gdsIDS = NaN(length(L),length(VGS));
 Av    = NaN(length(L),length(VGS));
 Av_db = NaN(length(L),length(VGS));
 VOV   = NaN(length(L),length(VGS));
 w     = NaN(length(L),length(VGS));
 ft    = nan(length(L), length(VGS));
+IDS   =  NaN(length(L),length(VGS));
 
-Mn2.vds = 0.550;
+Mn2.vds = 0.36;
 Mn2.vsb = 0;
 
 for kk = 1:length(L)
     Mn2.lg = L(kk);
-    Mn2.w = 10*Mn2.lg; %% W/L = 10
+    Mn2.w = 5*Mn2.lg; %% W/L = 10
     for i=1:length(VGS)
         Mn2.vgs = VGS(i);
         Mn2 = mosNfingers(Mn2);
@@ -65,14 +67,49 @@ for kk = 1:length(L)
         gm(kk,i) = Mn2.gm;
         gds(kk,i) = Mn2.gds;
         gmIDS(kk,i) = Mn2.gm/Mn2.ids;
+        gdsIDS(kk,i) = Mn2.gds/Mn2.ids;
         Av(kk,i) = Mn2.gm / Mn2.gds;
         Av_db(kk,i) = 20*log10(Mn2.gm / Mn2.gds);
         ft(kk, i) = Mn2.ft/1e9;
+        IDS(kk,i) = Mn2.ids;
                      
     end
 end
 
+%%
+
+Mn4.vgs = 0.36;
+Mn4.vds = 0.55;
+Mn4.vsb = 0;
+
+aspect = 80;
+Mn4.lg = 90e-9;
+Mn4.w = Mn4.lg*aspect;
+
+Mn4 = mosNfingers(Mn4);
+Mn4 = mosOpValues(Mn4);
+
+disp("currently: ")
+
+disp("ids = ")
+disp(Mn4.ids)
+disp("gds = ")
+disp(Mn4.gds)
+disp("gm * 10e3 = ")
+disp(Mn4.gm*1000)
+disp("Vov = ")
+disp(Mn4.vov)
+
+
 %% Plot
+
+figure;
+plot(VGS, VOV,'linewidth',2);
+xlabel('VGS (V)');
+ylabel('VOV (V)');
+grid on;
+title('VOV vs. VGS (W/L=10, VDS = 0.55 V)@')
+legend('60n','80n','100n','200n','500n','1000n');
 
 figure();
 subplot(221); plot(VGS,gm,'linewidth',2);
@@ -132,3 +169,18 @@ title('ft vs. VOV for different channel lenght (W/L=10, VDS = 0.55V)');
 legend('60n','80n','100n','200n','500n','1000n');
 xlabel('VOV (V)')
 ylabel('ft (GHz)')
+
+figure;
+plot(VGS.',IDS.','linewidth',2);
+title('IDS');
+legend('60n','80n','100n','200n','500n','1000n');
+xlabel('VOV (V)')
+ylabel('IDS')
+
+figure(); plot(VGS,gmIDS,'linewidth',2);
+xlabel('VGS (V)');
+ylabel('gds/IDS (mag)');
+grid on;
+title(' (gds/Ids) vs. VGS for different channel lenght (W/L=10, VDS = 0.55V)');
+legend('60n','80n','100n','200n','500n','1000n');
+
